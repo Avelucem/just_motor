a77b60567e409aa263dc042cd7e87b87b5d56164
Avelucem <Avelucem@gmail.com>
2017-10-26 06:52:50 +0300

    фикс
    фикс

--- just/court_2610/court_2610.py
+++ just/court_2610/court_2610.py
@@ -27,39 +27,47 @@ class Downloader(threading.Thread):
         inspector_soup = BeautifulSoup(inspector.content, 'lxml')
         try:
             DID = inspector_soup.find(id='did').next_element.split('_')[0]
+            if DID != u'0':
+                headers = {
+                "Host" : "court.gov.ua",
+                "Connection" : "keep-alive",
+                "Content-Length" : "35",
+                "Accept" : "text/plain, */*; q=0.01",
+                "Origin" : "http://court.gov.ua",
+                "X-Requested-With" : "XMLHttpRequest",
+                "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+                "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8",
+                "Referer" : url,
+                "Accept-Encoding" : "gzip, deflate",
+                "Accept-Language" : "en-US,en;q=0.8,ru;q=0.6,uk;q=0.4",
+                "Cookie" : "PHPSESSID=erl2dvg4g4jiksova3bqtfoao6"}
+                data = {"doc_ver" : "54_2",
+                "did" : DID,
+                "ddid" : "2610"}
+                req = Request('POST', url_logs, data=data, headers=headers)
+                prepped = req.prepare()
+                page = s.send(prepped)
+                soup = BeautifulSoup(page.content, 'lxml')
+                try:
+                    find_soup = soup.find('p').string.replace('\r',';').replace('I: ','').split('\n')
+                    f = open(os.path.abspath('just/court_2610/text.txt'), 'a+', encoding = 'utf-8')
+                    f.write(url)
+                    f.write(';')
+                    for index in find_soup[1:5]:
+                        f.write(index)
+                    f.write('\n')
+                    f.close()
+                    print('niceeeeeeeeeeeeeeeeeee%s'% url)
+                except:
+                    find_soup = '000000000000000000'
+                    f = open(os.path.abspath('just/court_2610/text.txt'), 'a+', encoding = 'utf-8')
+                    f.write(url)
+                    f.write(';')
+                    f.write('\n')
+                    f.close()
+                    print('_________________________________________________________%s '% url)
         except (AttributeError, NameError):
-            DID = 0
             print('FUCK' + url)
-        if DID != u'0':
-            f = open(os.path.abspath('just/court_2610/text.txt'), 'a+', encoding = 'utf-8')
-            headers = {
-            "Host" : "court.gov.ua",
-            "Connection" : "keep-alive",
-            "Content-Length" : "35",
-            "Accept" : "text/plain, */*; q=0.01",
-            "Origin" : "http://court.gov.ua",
-            "X-Requested-With" : "XMLHttpRequest",
-            "User-Agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
-            "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8",
-            "Referer" : url,
-            "Accept-Encoding" : "gzip, deflate",
-            "Accept-Language" : "en-US,en;q=0.8,ru;q=0.6,uk;q=0.4",
-            "Cookie" : "PHPSESSID=erl2dvg4g4jiksova3bqtfoao6"}
-            data = {"doc_ver" : "54_2",
-            "did" : DID,
-            "ddid" : "2610"}
-            req = Request('POST', url_logs, data=data, headers=headers)
-            prepped = req.prepare()
-            page = s.send(prepped)
-            soup = BeautifulSoup(page.content, 'lxml')
-            find_soup = soup.find('p').string.replace('\r',';').replace('I: ','').split('\n')
-            f.write(url)
-            f.write(';')
-            for index in find_soup[1:5]:
-                f.write(index)
-            f.write('\n')
-            f.close()
-            print('niceeeeeeeeeeeeeeeeeee%s'% url)
         print(url)
 
 def main(urls):
@@ -77,6 +85,6 @@ def main(urls):
 
 if __name__ == "__main__":
     urls = []
-    for links in list(range(int(last_number)+1, int(last_number)+10000)):
+    for links in list(range(int(last_number)+1, int(last_number)+1000000)):
         urls.append('http://court.gov.ua/log_documents/%s/2610/'% links)
     main(urls)
